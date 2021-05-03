b2c5b1611fa4677bc2a5342449f1dfd6cf36e761
jim-alexander <alexanderj2396@gmail.com>
2021-05-03 14:57:10 +1000

    icons

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,31 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { VitePWA } from 'vite-plugin-pwa'
 
+let manifest = {
+  start_url: '.',
+  theme_color: '#ffffff',
+  icons: [
+    {
+      src: '/src/assets/icons/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png'
+    },
+    {
+      src: '/src/assets/icons/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable'
+    }
+  ]
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
     VitePWA({
       registerType: 'autoUpdate',
-      manifest: {},
+      manifest,
       workbox: {}
     })
   ],
